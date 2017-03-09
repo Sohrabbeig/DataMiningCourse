316070eab713e45ffbd94423ed2798b5d99aadc8
sohrabbeig <ahsohrabbeig@gmail.com>
2017-03-09 09:56:26 +0330

    tiny change

--- HW2/src/Q1/drawPlots.py
+++ HW2/src/Q1/drawPlots.py
@@ -4,6 +4,7 @@ from scipy.stats.stats import pearsonr
 
 # load data
 train = pd.read_csv("../../description/1/train.csv", header=None, names=['G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7'])
+train.head()
 
 # plot
 for i in range(1,7):
