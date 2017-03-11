5ee8e3d43a75f071899a1239afdb576752cf8765
sohrabbeig <ahsohrabbeig@gmail.com>
2017-03-12 00:01:36 +0330

    lasso was too easy

--- HW2/src/Q1/newVersion/lasso.py
+++ HW2/src/Q1/newVersion/lasso.py
@@ -6,7 +6,7 @@ import math
 train = pd.read_csv("../train.csv", header=None, names=['G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7'])
 test = pd.read_csv("../test.csv", header=None, names=['G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6'])
 
-reg = linear_model.LinearRegression()
+reg = linear_model.Lasso(alpha=0.1)
 
 yTrain = train['G7'].as_matrix()
 del train['G7']
@@ -18,5 +18,5 @@ reg.fit(xTrain, yTrain)
 print('Coefficients: \n', reg.coef_)
 
 # The mean squared error
-print("Mean squared error: %.2f"
+print("RootMean squared error: %.2f"
       % math.sqrt(np.mean((reg.predict(xTrain) - yTrain) ** 2)))
