a607d02f32132b19d585fe2d61dc8f832db3cb2c
sohrabbeig <ahsohrabbeig@gmail.com>
2017-03-12 21:06:14 +0330

    10fold cross validation

--- HW2/src/Q1/newVersion/gradienBoosting.py
+++ HW2/src/Q1/newVersion/gradienBoosting.py
@@ -2,18 +2,32 @@ from sklearn import ensemble
 from sklearn.metrics import mean_squared_error
 import pandas as pd
 import math
+import numpy as np
+from sklearn.model_selection import KFold
 
 train = pd.read_csv("../train.csv", header=None, names=['G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7'])
-test = pd.read_csv("../test.csv", header=None, names=['G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6'])
+# del train['G0']
+t = np.asarray(train)
 
-yTrain = train['G7'].as_matrix()
-del train['G7']
-xTrain = train.as_matrix()
+params = {'n_estimators': 500, 'max_depth': 3, 'min_samples_split': 2,
+          'learning_rate': 0.005, 'loss': 'ls'}
 
-params = {'n_estimators': 500, 'max_depth': 4, 'min_samples_split': 2,
-          'learning_rate': 0.01, 'loss': 'ls'}
-clf = ensemble.GradientBoostingRegressor(**params)
-
-clf.fit(xTrain, yTrain)
-mse = mean_squared_error(yTrain, clf.predict(xTrain))
-print("RMSE: %.4f" % math.sqrt(mse))
+kf = KFold(n_splits=10)
+RMSE = 0
+for tr, te in kf.split(t):
+    xtr = []
+    ytr = []
+    xte = []
+    yte = []
+    for i in tr:
+        xtr.append(t[i, [0, 1, 2, 3, 4, 5]])
+        ytr.append(t[i, [6]])
+    clf = ensemble.GradientBoostingRegressor(**params)
+    clf.fit(np.asarray(xtr), np.asarray(ytr))
+    for j in te:
+        xte.append(t[j, [0, 1, 2, 3, 4, 5]])
+        yte.append(t[j, [6]])
+    mse = mean_squared_error(np.asarray(yte), clf.predict(np.asarray(xte)))
+    RMSE += math.sqrt(mse)
+RMSE /= 10
+print(RMSE)
